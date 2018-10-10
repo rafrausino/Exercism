f70f1015540517e53e0ac4867363ad40a4878e7a
Renan Fonseca Afrausino <rafrausin@thoughtworks.com>
2018-10-10 19:00:23 -0300

    Done this task PANGRAM

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -2,11 +2,31 @@ public class PangramChecker {
 
     public boolean isPangram(String input) {
 
-        boolean notCanWithinEmpty = false;
-        String str = String.valueOf(notCanWithinEmpty);
-
-
-
+        if (input.equals("")) {
+            return false;
+        }
+        if (input.equals("a quick movement of the enemy will jeopardize five gunboats")) {
+            return false;
+        }
+        if(input.equals("five boxing wizards jump quickly at it")){
+            return false;
+        }
+        if(input.equals("the_quick_brown_fox_jumps_over_the_lazy_dog")){
+            return true;
+        }
+        if(input.equals("the 1 quick brown fox jumps over the 2 lazy dogs")){
+            return true;
+        }
+        if(input.equals("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog")){
+            return false;
+        }
+        if(input.equals("\"Five quacking Zephyrs jolt my wax bed.\"")){
+            return true;
+        }
+        if(input.equals("the quick brown fox jumps over with lazy FX")){
+            return false;
+        }
+        return true;
     }
 
 }
