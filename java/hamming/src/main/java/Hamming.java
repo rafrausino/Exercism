a67354b6a47c3d12ed925b080a2672235558db88
Renan Fonseca Afrausino <rafrausin@thoughtworks.com>
2018-10-10 18:59:15 -0300

    Estou fazendo Hamming ainda

---
+++ java/hamming/src/main/java/Hamming.java
@@ -0,0 +1,27 @@
+class Hamming {
+
+    Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.length() != rightStrand.length());
+    }
+
+    int getHammingDistance() {
+
+        int hammingDistance = 0;
+
+        for (int i = 0; i < hammingDistance; i++) {
+
+            String distanceLeft = String.valueOf(i);
+
+            if (distanceLeft.charAt(i) != distanceLeft.charAt(i)) {
+                hammingDistance++;
+            }
+        }
+//        if(hammingDistance != 0){
+//        }
+
+        return hammingDistance;
+
+
+    }
+
+}
