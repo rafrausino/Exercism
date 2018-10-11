900d26f75206562d599d426b7b09d25f061e985e
Renan Fonseca Afrausino <rafrausin@thoughtworks.com>
2018-10-11 19:49:00 -0300

    Sloved this exercise HUMMING

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,27 +1,37 @@
 class Hamming {
 
+    String leftStrand;
+    String rightStrand;
+
+
     Hamming(String leftStrand, String rightStrand) {
-        if (leftStrand.length() != rightStrand.length());
+
+        if (leftStrand.length()!= rightStrand.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+
     }
 
     int getHammingDistance() {
 
-        int hammingDistance = 0;
+        if (leftStrand.length() != rightStrand.length()) {
+            return 0;
+        }
 
-        for (int i = 0; i < hammingDistance; i++) {
-
-            String distanceLeft = String.valueOf(i);
-
-            if (distanceLeft.charAt(i) != distanceLeft.charAt(i)) {
-                hammingDistance++;
+        int diferent = 0;
+        for (int i = 0; i < leftStrand.length(); i++) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
+                diferent += 1;
             }
         }
-//        if(hammingDistance != 0){
-//        }
 
-        return hammingDistance;
-
-
+        return diferent;
     }
 
 }
+
+
+
