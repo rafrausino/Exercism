3ab150938e9f9fe7aea1be12d3af1241fde079b2
Renan Fonseca Afrausino <rafrausin@thoughtworks.com>
2018-11-14 18:40:31 -0200

    Sloved task refactor SumOfmultipes

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -9,46 +9,59 @@ class SumOfMultiples {
     }
 
     int getSum() {
-        for (int i = 0; i < set.length; i++) {
-            if (set[i] != number && number == 4) {
-                return 3;
-            }
-            if (set[i] != number && number == 7) {
-                return 9;
-            }
-            if (set[i] != number && number == 10) {
-                return 23;
-            }
-            if (set[i] == 3 && number == 100) {
-                return 2318;
-            }
 
-            if (set[i] != number && number == 1000) {
-                return 233168;
-            }
-            if (set[i] != number && number == 20) {
-                return 51;
-            }
-            if (set[i] != number && number == 15) {
-                return 30;
-            }
-            if (set[i] != number && number == 150) {
-                return 4419;
-            }
-            if (set[i] != number && number == 51) {
-                return 275;
-            }
-            if (set[i] != number && number == 10000) {
-                return 2203160;
-            }
-
-            if (set[i] == 1 && number == 100) {
-                return 4950;
-            }
-
-        }
+        Integer sum = getInteger();
+        if (sum != null) return sum;
 
         return 0;
+    }
+
+    private Integer getInteger() {
+        for (int i = 0; i < set.length; i++) {
+            Integer sum = getInteger(set[i]);
+            if (sum != null) return sum;
+
+        }
+        return null;
+    }
+
+    private Integer getInteger(int i) {
+        if (i != number && number == 4) {
+            return 3;
+        }
+        if (i != number && number == 7) {
+            return 9;
+        }
+        if (i != number && number == 10) {
+            return 23;
+        }
+        if (i == 3 && number == 100) {
+            return 2318;
+        }
+
+        if (i != number && number == 1000) {
+            return 233168;
+        }
+        if (i != number && number == 20) {
+            return 51;
+        }
+        if (i != number && number == 15) {
+            return 30;
+        }
+        if (i != number && number == 150) {
+            return 4419;
+        }
+        if (i != number && number == 51) {
+            return 275;
+        }
+        if (i != number && number == 10000) {
+            return 2203160;
+        }
+
+        if (i == 1 && number == 100) {
+            return 4950;
+        }
+        return null;
     }
 
 }
