14628494beaa25b7016145d18fe555abc7962905
Renan Fonseca Afrausino <rafrausin@thoughtworks.com>
2018-12-11 18:43:05 -0200

    I am cleaning a file largestSerieProduct

---
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -0,0 +1,19 @@
+class LargestSeriesProductCalculator {
+
+    private String inputNumber;
+
+    LargestSeriesProductCalculator(String inputNumber) {
+        this.inputNumber = inputNumber;
+        String[] splitNumber = inputNumber.split(" ");
+        
+
+
+
+
+        throw new UnsupportedOperationException ("Delete this statement and write your own implementation.");
+    }
+
+    long calculateLargestProductForSeriesLength(int numberOfDigits) {
+        throw new UnsupportedOperationException ("Delete this statement and write your own implementation.");
+    }
+}
